0ae5b7e162fcdf3e07b08423df7257db0a214e3f
srcrook <srcrook@westernpower.co.uk>
2021-07-12 09:33:39 +0100

    Trying to reconstruct govmomi example

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/crooks/vmware_exporter
 
 go 1.16
 
-require gopkg.in/yaml.v2 v2.4.0 // indirect
+require (
+	github.com/crooks/vmware_exporter/config v0.0.0-20210711171556-a1e15aa410b9
+	github.com/vmware/govmomi v0.26.0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,34 +4,62 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/url"
 	"github.com/crooks/vmware_exporter/config"
+	"github.com/vmware/govmomi/session/cache"
 	"github.com/vmware/govmomi/vim25"
 	"github.com/vmware/govmomi/vim25/mo"
 	"github.com/vmware/govmomi/view"
+	"github.com/vmware/govmomi/vim25/soap"
 )
 
+var (
+	cfg *config.Config
+)
+
+func newClient(ctx context.Context) (*vim25.Client, error) {
+	u, err := soap.ParseURL(cfg.API.URL)
+	u.User = url.UserPassword(cfg.API.UserID, cfg.API.Password)
+	s := &cache.Session{
+		URL: u,
+		Insecure: cfg.API.Insecure,
+	}
+
+	c := new(vim25.Client)
+	err = s.Login(ctx, c, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
 func main() {
+	var err error
 	flags := config.ParseFlags()
-	cfg, err := config.ParseConfig(flags.ConfigFile)
+	cfg, err = config.ParseConfig(flags.ConfigFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(cfg.API.UserID)
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	ctx := context.Background()
+    c, err := newClient(ctx)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	m := view.NewManager(ctx)
+	m := view.NewManager(c)
 	v, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"HostSystem"}, true)
     if err != nil {
-        return err
+        log.Fatal(err)
     }
     defer v.Destroy(ctx)
 
 	var hss []mo.HostSystem
 	err = v.Retrieve(ctx, []string{"HostSystem"}, []string{"summary"}, &hss)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
 	for _, hs := range hss {
 		fmt.Println(hs)
